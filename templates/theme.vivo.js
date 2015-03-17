b4d1ae4b0d29085dee8ba934ab1cc529dfae316f
Borja Illescas Medina <billescas@conwet.com>
2015-03-17 12:19:55 +0100

    Now the script works in firefox too. Before it didn't wait Polymer to load core-style, now it does

--- templates/theme.vivo.js
+++ templates/theme.vivo.js
@@ -1,2 +1,11 @@
-include "variables.less"
+//Function that sets the CoreStyle variables
+var setVars = function(){
+	include "variables.less"
+}
+
+if (typeof CoreStyle === 'undefined'){//If core-style has not been loaded we set setVars() to execute when polymer is ready
+	window.addEventListener('polymer-ready', setVars);
+}else{//If core-style has already been loaded we execute setVars() 
+	setVars();
+}
 

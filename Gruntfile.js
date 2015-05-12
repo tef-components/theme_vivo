8f0b727fe71f1c37fa007a0fbeb0b2d5822cec88
Arnau March <arnau@arnaumarch.com>
2015-05-12 10:38:44 +0200

    restructured files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,23 +5,7 @@ module.exports = function(grunt) {
     less: {
       default: {
         files: {
-          'css/components.vivo.css': 'less/theme.less',
-          'templates/variables.css': 'templates/variables.less',
-          'css/components/buttons.css': 'less/buttons.less',
-          'css/web_components/tef_button.css': 'less/tef_button.less',
-          'css/components/button-groups.css': 'less/button-groups.less',
-          'css/components/dropdowns.css': 'less/dropdowns.less',
-          'css/components/inputs.css': 'less/inputs.less',
-          'css/components/radios.css': 'less/radios.less',
-          'css/components/checkboxes.css': 'less/checkboxes.less',
-          'css/components/modals.css': 'less/modals.less',
-          'css/components/headers.css': 'less/headers.less',
-          'css/components/sidebars.css': 'less/sidebars.less',
-          'css/components/tabs.css': 'less/tabs.less',
-          'css/components/toolbars.css': 'less/toolbars.less',
-          'css/components/tables.css': 'less/tables.less',
-          'css/components/tooltips.css': 'less/tooltips.less',
-          'css/components/lists.css': 'less/lists.less'
+          'css/components.vivo.css': 'less/theme.less'
         }
       }
     },
@@ -59,90 +43,8 @@ module.exports = function(grunt) {
       },
       target: {
         files: {
-          'css/components.vivo.min.css': 'css/components.vivo.css',
-          'css/components/buttons.min.css': 'css/components/buttons.css',
-          'css/web_components/tef_button.min.css': 'css/web_components/tef_button.css',
-          'css/components/button-groups.min.css': 'css/components/button-groups.css',
-          'css/components/dropdowns.min.css': 'css/components/dropdowns.css',
-          'css/components/inputs.min.css': 'css/components/inputs.css',
-          'css/components/radios.min.css': 'css/components/radios.css',
-          'css/components/checkboxes.min.css': 'css/components/checkboxes.css',
-          'css/components/modals.min.css': 'css/components/modals.css',
-          'css/components/headers.min.css': 'css/components/headers.css',
-          'css/components/sidebars.min.css': 'css/components/sidebars.css',
-          'css/components/tabs.min.css': 'css/components/tabs.css',
-          'css/components/toolbars.min.css': 'css/components/toolbars.css',
-          'css/components/tables.min.css': 'css/components/tables.css',
-          'css/components/tooltips.min.css': 'css/components/tooltips.css',
-          'css/components/lists.min.css': 'css/components/lists.css'
+          'css/components.vivo.min.css': 'css/components.vivo.css'
         }
-      }
-    },
-
-    // reformating  variables.less to create a core-styles compliant file
-    // to be used in Web components
-    replace: {
-      preless: {
-        options: {
-          patterns: [
-            { match: '    @', replacement: '.CoreStyle-' },
-            { match: ': ', replacement: ' { fake-property: ' },
-            { match: ';', replacement: '; }' }
-          ],
-          usePrefix: false
-        },
-        files: [
-          {
-            expand: true, flatten: true,
-            src: ['less/variables.less'], dest: 'templates/'
-          }
-        ]
-      },
-
-      import: {
-        options: {
-          patterns: [
-            {
-              match: '// Variables',
-              replacement: '@import "../less/variables.less";'
-            }
-          ],
-          usePrefix: false
-        },
-        files: [
-          {
-            expand: true, flatten: true,
-            src: ['templates/variables.less'], dest: 'templates/'
-          }
-        ]
-      },
-
-      final: {
-        options: {
-          patterns: [
-            { match: '.CoreStyle-', replacement: 'CoreStyle.g.' },
-            { match: '{', replacement: '=' },
-            { match: 'fake-property: ', replacement: '"' },
-            { match: ';', replacement: '";' },
-            { match: '}', replacement: '' },
-          ],
-          usePrefix: false
-        },
-        files: [
-          {
-            expand: true, flatten: true,
-            src: ['templates/variables.css'], dest: 'templates/'
-          }
-        ]
-      }
-    },
-
-    // create theme*.js to to be used in Web Components
-    includes: {
-      files: {
-        cwd: 'templates/',
-        src: '**/*.js',
-        dest: ''
       }
     },
 
@@ -196,8 +98,8 @@ module.exports = function(grunt) {
 
     watch: {
       styles: {
-        files: ['../**/less/*.less'],
-        tasks: ['less', 'concat'],
+        files: ['../**/*.less'],
+        tasks: ['less','autoprefixer','cssmin'],
         options: {
           nospawn: true
         }
@@ -211,27 +113,12 @@ module.exports = function(grunt) {
     'watch'
   ]);
 
-  grunt.registerTask('webcomponents', [
-    'replace:preless',
-    'replace:import',
-    'less',
-    'replace:final',
-    'autoprefixer',
-    'concat',
-    'cssmin',
-    'includes'
-  ]);
-
   grunt.registerTask('release', [
     'copy',
-    'replace:preless',
-    'replace:import',
     'less',
-    'replace:final',
     'autoprefixer',
     'concat',
     'cssmin',
-    'includes',
     'exec:add',
     'prompt',
     'exec:message',
